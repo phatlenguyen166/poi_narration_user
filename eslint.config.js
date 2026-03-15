f29d4a390bda45c1ee0c8736b45a3dcabfcd9270
phatlenguyen166 <phatlenguyen166@gmail.com>
2026-03-15 21:32:48 +0700

    setup(project): install prettier, eslint check

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,45 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'vite.config.ts', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig
     ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    plugins: {
+      prettier: prettierPlugin
     },
-  },
+    languageOptions: {
+      ecmaVersion: 2024,
+      globals: globals.browser
+    },
+    rules: {
+      // Dán đoạn cấu hình của bạn vào đây
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ],
+      // Các rule khác của bạn (nếu có)
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+    }
+  }
 ])
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
 })
