e3c097f7faa050d4bc85bb15e64c002287c96880
phatlenguyen166 <phatlenguyen166@gmail.com>
2026-03-15 21:35:58 +0700

    setup(project): install tailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react(), tailwindcss(), babel({ presets: [reactCompilerPreset()] })],
   server: {
     port: 3000
   },
